e87210e9073a858a0b188c2775918940c8d098e9
Leslie M <lesliemariemac@gmail.com>
2023-11-24 08:45:23 -0500

    Added budget calculator with user input and calculate function

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//Select paragraph and button
+var tripInfoButton = document.querySelector(".trip-info"); 
+var dailyBudget = document.querySelector(".daily-budget");
+
+//Function to get user input
+var tripInfo = function() {
+    var totalBudget = Number(
+        prompt("What is your total budget for this trip?")
+    );
+    var accommodation = Number(
+        prompt("What are your accommodation costs?")
+    );
+    var numDays = Number(
+        prompt("How many days does your trip last?")
+    );
+    calculateDailyBudget(totalBudget, accommodation, numDays);
+};
+
+
+//Function to Calculate Daily Budget
+var calculateDailyBudget = function (totalBudget, accommodation, numDays) {
+    var daily = ((totalBudget - accommodation) / numDays).toFixed(2);
+    dailyBudget.innerText = `You can spend $${daily} a day on food and fun!`;
+};
+
+tripInfoButton.addEventListener("click", tripInfo);
